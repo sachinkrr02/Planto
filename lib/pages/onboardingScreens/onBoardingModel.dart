a4c8a0fba67d52d164aebe32cfbfd4a4752090ae
Sachin <sachin2013086@akgec.ac.in>
2023-12-29 03:34:26 +0530

    Onboarding Screen

---
+++ lib/pages/onboardingScreens/onBoardingModel.dart
@@ -0,0 +1,14 @@
+class OnBoard {
+  final String image, desc;
+
+  OnBoard({
+    required this.image,
+    required this.desc,
+  });
+}
+
+final List<OnBoard> demo_data = [
+  OnBoard(image: "assets/p1.jpg", desc: "Enjoy Life With Plants.."),
+  OnBoard(image: "assets/p2.jpg", desc: "Greener With Every tap.."),
+  OnBoard(image: "assets/p3.jpg", desc: "Leaf by Leaf Grow Smart.."),
+];
